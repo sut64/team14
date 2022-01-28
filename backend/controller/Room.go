4f57cbb0c7d73ae413a1d54d3570c6b476b9e799
pokpak101 <pokpak10145@gmail.com>
2022-01-28 19:54:39 +0700

    close #115 - implement room controller

---
+++ backend/controller/Room.go
@@ -0,0 +1,81 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/chanwit/sa-64-example/entity"
+	"github.com/gin-gonic/gin"
+)
+// POST /users
+func CreateRoom(c *gin.Context) {
+	var room entity.Room
+	if err := c.ShouldBindJSON(&room); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := entity.DB().Create(&room).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": room})
+}
+
+// GET /user/:id
+func GetRoom(c *gin.Context) {
+	var room entity.Room
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM rooms WHERE id = ?", id).Scan(&room).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": room})
+}
+
+// GET /users
+
+func ListRooms(c *gin.Context) {
+	var rooms []entity.Room
+	if err := entity.DB().Raw("SELECT * FROM rooms").Scan(&rooms).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": rooms})
+}
+
+// DELETE /users/:id
+
+func DeleteRoom(c *gin.Context) {
+	id := c.Param("id")
+	if tx := entity.DB().Exec("DELETE FROM rooms WHERE id = ?", id); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "rooms not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": id})
+}
+
+// PATCH /users
+
+func UpdateRoom(c *gin.Context) {
+	var room entity.Room
+	if err := c.ShouldBindJSON(&room); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if tx := entity.DB().Where("id = ?", room.ID).First(&room); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "rooms not found"})
+		return
+	}
+
+	if err := entity.DB().Save(&room).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": room})
+}

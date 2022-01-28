146a34900bbb0e1b2de9694192d257d6943ddf6c
Worrakarnp <pworrakarn@gmail.com>
2022-01-28 14:18:03 +0700

    close #21 - ทำ entity Specialist

---
+++ backend/entity/Specialist.go
@@ -0,0 +1,16 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+)
+
+type Specialist struct {
+	gorm.Model
+	Specialist   string
+	Name         string
+	Tel          string
+	Email        string
+	Prevention   []Prevention   `gorm:"foreignKey:SpecialistID"`
+	Appointment  []Appointment  `gorm:"foreignKey:SpecialistID"`
+	RoomDataList []RoomDataList `gorm:"foreignKey:SpecialistID"`
+}

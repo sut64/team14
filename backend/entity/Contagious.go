fdcd8435fb9937fa5822634da2fd0477199f90c8
Jirawun Phukhokkhoy <b6132200@g.sut.ac.th>
2022-02-04 12:31:50 +0700

    close #8 - implement entity Contagious

---
+++ backend/entity/Contagious.go
@@ -0,0 +1,28 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Contagious struct {
+	gorm.Model
+	Name       string `valid:"required~Name cannot be blank"`
+	Symptom    string
+	Incubation int
+	Date       time.Time
+
+	GermID *uint
+	Germ   Germ `gorm:"references:id" valid:"-"`
+
+	CatchingTypeID *uint
+	CatchingType   CatchingType `gorm:"references:id" valid:"-"`
+
+	RiskGroupTypeID *uint
+	RiskGroupType   RiskGroupType `gorm:"references:id" valid:"-"`
+
+	Prevention         []Prevention         `gorm:"foreignKey:ContagiosID"`
+	MedicineandVaccine []MedicineandVaccine `gorm:"foreignKey:ContagiosID"`
+}
+
